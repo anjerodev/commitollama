85391a0cba6763a7aad138b48e1a404502e162fc
Jepri <jepri.creations@gmail.com>
2024-09-30 11:57:48 +0700

    test: Adding tests

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,12 @@
-import { defineConfig } from '@vscode/test-cli';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { defineConfig } from "@vscode/test-cli";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-	files: 'out/test/**/*.test.js',
+	files: "out/test/**/*.test.js",
+	extensionDevelopmentPath: __dirname,
+	workspaceFolder: `${__dirname}/sampleWorkspace`,
 });
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,2 +1,152 @@
-// import * as assert from 'assert';
-// import * as vscode from 'vscode';
+import * as assert from 'node:assert'
+import * as vscode from 'vscode'
+import * as sinon from 'sinon'
+import * as extension from '../extension'
+import * as ollama from 'ollama'
+import { getSummary, getCommitMessage } from '../generator'
+import { defaultConfig } from '../config'
+
+suite('Extension Test Suite', () => {
+	test('Extension is active', () => {
+		assert.ok(extension.activate)
+	})
+
+	test('Get Git Extension', () => {
+		const gitExtension = extension.getGitExtension()
+		assert.ok(gitExtension)
+	})
+})
+
+suite('getSummary Tests', () => {
+	const diffSample =
+		'diff --git a/sample.txt b/sample.txt\nindex 83db48f..1f65a11 100644\n--- a/sample.txt\n+++ b/sample.txt\n@@ -1 +1 @@\n-Hello\n+Hello World'
+	const summaryResponse = {
+		message: { content: 'Added a Hello World message.' },
+	}
+
+	let ollamaChatStub: sinon.SinonStub
+
+	setup(() => {
+		ollamaChatStub = sinon.stub(ollama.Ollama.prototype, 'chat')
+	})
+
+	teardown(() => {
+		ollamaChatStub.restore()
+	})
+
+	test('Should return a summary for a git diff output', async () => {
+		ollamaChatStub.resolves(summaryResponse)
+
+		const result = await getSummary(diffSample)
+
+		assert.strictEqual(result, 'Added a Hello World message.')
+		assert(ollamaChatStub.calledOnce)
+	})
+
+	test('Should show error message when model is not found', async () => {
+		const error = { status_code: 404, message: 'model not found' }
+		ollamaChatStub.rejects(error)
+
+		const showErrorMessageStub = sinon
+			.stub(vscode.window, 'showErrorMessage')
+			.resolves()
+
+		try {
+			await getSummary(diffSample)
+		} catch (e) {
+			// Expected error
+		}
+
+		assert(
+			showErrorMessageStub.calledOnceWith(
+				sinon.match.string,
+				sinon.match.string,
+				sinon.match.string,
+			),
+		)
+		showErrorMessageStub.restore()
+	})
+})
+
+suite('getCommitMessage Tests', () => {
+	const summariesSample = ['Added a feature', 'Fixed a bug']
+	const commitMessageResponse = {
+		message: { content: 'feat: Add new feature' },
+	}
+
+	let ollamaChatStub: sinon.SinonStub
+
+	setup(() => {
+		ollamaChatStub = sinon.stub(ollama.Ollama.prototype, 'chat')
+	})
+
+	teardown(() => {
+		ollamaChatStub.restore()
+	})
+
+	test('Should return a commit message based on summaries', async () => {
+		ollamaChatStub.resolves(commitMessageResponse)
+
+		const result = await getCommitMessage(summariesSample)
+
+		assert.strictEqual(result, 'feat: Add new feature')
+		assert(ollamaChatStub.calledOnce)
+	})
+
+	test('Should add emojis if configured to use emojis', async () => {
+		ollamaChatStub.resolves(commitMessageResponse)
+
+		const config = vscode.workspace.getConfiguration('commitollama')
+		const originalEmojis = config.commitEmojis
+		const originalUseEmojis = config.useEmojis
+
+		await config.update(
+			'commitEmojis',
+			{
+				...defaultConfig.commitEmojis,
+				feat: '🔥',
+			},
+			vscode.ConfigurationTarget.Workspace,
+		)
+		await config.update('useEmojis', true, vscode.ConfigurationTarget.Workspace)
+
+		const result = await getCommitMessage(summariesSample)
+
+		assert.strictEqual(result, 'feat 🔥: Add new feature')
+		await config.update(
+			'commitEmojis',
+			originalEmojis,
+			vscode.ConfigurationTarget.Workspace,
+		)
+		await config.update(
+			'useEmojis',
+			originalUseEmojis,
+			vscode.ConfigurationTarget.Workspace,
+		)
+	})
+
+	test('Should add summaries as descriptions if configured to use descriptions', async () => {
+		ollamaChatStub.resolves(commitMessageResponse)
+		const config = vscode.workspace.getConfiguration('commitollama')
+
+		const originalUseDescription = config.useDescription
+		await config.update(
+			'useDescription',
+			true,
+			vscode.ConfigurationTarget.Workspace,
+		)
+
+		const result = await getCommitMessage(summariesSample)
+
+		assert.strictEqual(
+			result,
+			'feat: Add new feature\n\n- Added a feature\n- Fixed a bug',
+		)
+
+		await config.update(
+			'useDescription',
+			originalUseDescription,
+			vscode.ConfigurationTarget.Workspace,
+		)
+	})
+})
