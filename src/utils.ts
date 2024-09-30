9d1b12ba52a0b269a7f67d2ea7bd3b15fd1f63a8
Jepri <jepri.creations@gmail.com>
2024-09-30 12:18:08 +0700

    refactor: function reorganization

---
+++ src/utils.ts
@@ -0,0 +1,52 @@
+import * as vscode from 'vscode'
+import type { GitExtension, Repository } from './types/git'
+import { getCommitMessage, getSummary } from './generator'
+
+export async function getSummaryUriDiff(repo: Repository, uri: string) {
+	const diff = await repo.diffIndexWithHEAD(uri)
+	const summary = await getSummary(diff)
+	return summary
+}
+
+export async function createCommitMessage(repo: Repository) {
+	vscode.window.withProgress(
+		{
+			location: vscode.ProgressLocation.SourceControl,
+			cancellable: false,
+			title: 'Loading commit message',
+		},
+		async () => {
+			vscode.commands.executeCommand('workbench.view.scm')
+			try {
+				// Clean the current message:
+				repo.inputBox.value = ''
+
+				const ind = await repo.diffIndexWithHEAD()
+
+				if (ind.length === 0) {
+					throw new Error(
+						'No changes to commit. Please stage your changes first.',
+					)
+				}
+
+				const callbacks = ind.map((change) =>
+					getSummaryUriDiff(repo, change.uri.fsPath),
+				)
+				const summaries = await Promise.all(callbacks)
+
+				const commitMessage = await getCommitMessage(summaries)
+				repo.inputBox.value = commitMessage
+			} catch (error: any) {
+				vscode.window.showErrorMessage(
+					error?.message || 'Unable to create commit message.',
+				)
+			}
+		},
+	)
+}
+
+export function getGitExtension() {
+	const vscodeGit = vscode.extensions.getExtension<GitExtension>('vscode.git')
+	const gitExtension = vscodeGit?.exports
+	return gitExtension?.getAPI(1)
+}
